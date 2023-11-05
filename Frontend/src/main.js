9c4d3bcc4035e7b66a1d3f5c55bb282d32d02fd9
Андрій Ачілов <andrry1@gmail.com>
2023-11-06 01:44:52 +0200

    Delete code from the hello-world vue project

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import '/src/assets/styles.scss'
+import "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createApp(App).mount('#app')
