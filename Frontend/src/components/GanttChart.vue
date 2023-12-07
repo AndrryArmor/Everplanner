2b9ad788939ddc643ec45f3b165dee40fb3de196
Андрій Ачілов <andrry1@gmail.com>
2023-12-07 02:13:07 +0200

    Add data going by props; add back navigation

--- Frontend/src/components/GanttChart.vue
+++ Frontend/src/components/GanttChart.vue
@@ -2,11 +2,13 @@
   <div class="container-fluid text-center">
     <h4>Діаграма Ганта для проєкту:</h4>
     <Bar id="my-chart-id" :options="chartOptions" :data="chartData" :style="style" />
+    <button type="button" class="btn btn-lg btn-primary m-2" @click="backToPlanning">
+      Назад до планування проєкту
+    </button>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
 import { Bar } from "vue-chartjs";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import {
@@ -18,43 +20,11 @@ import {
   CategoryScale,
   LinearScale,
 } from "chart.js";
-
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartDataLabels);
 
-const chartData = {
-  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-  datasets: [
-    {
-      datalabels: {
-        labels: {
-          title: null,
-        },
-      },
-      data: [0, 0, 0, 2.5, 3.333, 6, 5.5, 6, 8.333, 9],
-      backgroundColor: "rgba(0, 0, 0, 0)",
-    },
-    {
-      label: "1",
-      data: [0, 3.333, 0, 0, 2.667, 2.333, 0, 0, 0.667, 1.667],
-      backgroundColor: "rgb(255, 160, 100)",
-    },
-    {
-      label: "2",
-      data: [2.5, 0, 0, 3, 0, 0, 0, 2, 0, 0],
-      backgroundColor: "rgb(160, 120, 180)",
-    },
-    {
-      label: "3",
-      data: [0, 0, 3.333, 0, 0, 0, 1.667, 0, 0, 0],
-      backgroundColor: "rgb(120, 255, 40)",
-    },
-    {
-      label: "4",
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      backgroundColor: "rgb(70, 90, 255)",
-    },
-  ],
-};
+const props = defineProps(["chartData"]);
+const emit = defineEmits(["backToPlanning"]);
+
 const chartOptions = {
   responsive: true,
   indexAxis: "y",
@@ -116,4 +86,8 @@ const style = {
   width: "1500px",
   position: "relative",
 };
+
+function backToPlanning() {
+  emit("backToPlanning");
+}
 </script>
