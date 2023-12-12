6b798cc1a15cf18f41d1bd11c69fdf60ae201b41
Андрій Ачілов <andrry1@gmail.com>
2023-12-12 16:38:58 +0200

    Fix login markup; add validation and navigation to it.

--- Frontend/src/components/TheLogin.vue
+++ Frontend/src/components/TheLogin.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="container my-3">
     <div class="row justify-content-center">
-      <div class="col-sm-10 login-wrapper">
-        <form class="container border rounded rounded-5 login">
+      <div class="col-sm-auto" style="width: 550px">
+        <form
+          class="row row-cols-1 border rounded rounded-5 needs-validation"
+          novalidate
+          v-on:submit.prevent="login($event)"
+        >
           <h1 class="fw-light my-4 text-center">Вхід в Everplanner</h1>
-          <div class="row mb-4 mx-1">
-            <label for="inputEmail" class="col-12 col-form-label fs-5">Email</label>
-            <div class="col-12">
-              <input type="email" class="form-control form-control-lg" id="inputEmail" />
-            </div>
+          <div class="col mb-4">
+            <label for="inputEmail" class="form-label fs-5">Електронна пошта</label>
+            <input
+              v-model="email"
+              type="email"
+              class="form-control form-control-lg"
+              id="inputEmail"
+              placeholder="example@email.com"
+              required
+            />
+            <div class="invalid-feedback">Будь ласка, введіть електронну пошту.</div>
           </div>
-          <div class="row mb-4 mx-1">
-            <label for="inputPassword" class="col-12 col-form-label fs-5">Пароль</label>
-            <div class="col-12">
-              <input type="password" class="form-control form-control-lg" id="inputPassword" />
-            </div>
+          <div class="col mb-4">
+            <label for="inputPassword" class="form-label fs-5">Пароль</label>
+            <input
+              v-model="password"
+              type="password"
+              class="form-control form-control-lg"
+              id="inputPassword"
+              required
+            />
+            <div class="invalid-feedback">Будь ласка, введіть ім'я.</div>
           </div>
-          <div class="row mb-3 mx-1">
-            <div class="col">
-              <button type="submit" class="btn btn-lg btn-success w-100">Ввійти</button>
-            </div>
+          <div class="col mb-3">
+            <button type="submit" class="btn btn-lg btn-success w-100">Ввійти</button>
           </div>
-          <div class="row mb-4 mx-1">
-            <div class="col text-center">
-              <span>Ви тут вперше? Тоді <router-link to="/signup" href="#">зареєструйтесь</router-link></span>
-            </div>
+          <div class="col mb-4 text-center">
+            <span
+              >Ви тут вперше? Тоді
+              <router-link to="/signup" href="#">зареєструйтесь</router-link></span
+            >
           </div>
         </form>
       </div>
@@ -32,10 +46,20 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+import router from "../router";
 
-<style scoped lang="scss">
-.login-wrapper {
-  width: 550px;
+const email = ref("");
+const password = ref("");
+
+function login(event) {
+  const form = event.target;
+  form.classList.add("was-validated");
+  if (form.checkValidity()) {
+    router.push("/projects");
+  }
 }
-</style>
+</script>
+
+<style scoped lang="scss"></style>
