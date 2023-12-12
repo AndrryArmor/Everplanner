6c0648c4b45966aa41a68f7d26361ca4236d9a84
Андрій Ачілов <andrry1@gmail.com>
2023-12-12 04:30:03 +0200

    Fix and simplify navbar code

--- Frontend/src/components/TheNavbar.vue
+++ Frontend/src/components/TheNavbar.vue
@@ -1,22 +1,29 @@
 <template>
-  <nav class="navbar shadow bg-gradient bg-light">
+  <nav class="navbar navbar-expand-sm shadow bg-gradient bg-light">
     <div class="container-fluid">
       <router-link to="/login" class="navbar-brand text-success fw-light">
         Everplanner
       </router-link>
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <router-link to="/projects" class="nav-link active">Головна</router-link>
-        </li>
-      </ul>
-      <ul class="navbar-nav">
-        <li class="nav-item">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav me-auto">
+          <router-link to="/projects" class="nav-link">Проєкти</router-link>
+        </div>
+        <div class="navbar-nav">
+          <span class="nav-link active user-select-none">Ачілов Андрій</span>
           <router-link to="/login" class="btn btn-outline-danger" role="button">
             Вийти
             <i class="bi bi-box-arrow-right"></i>
           </router-link>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
