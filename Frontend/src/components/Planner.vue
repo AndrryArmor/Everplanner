282fc053d899ac52d1d4e3b80f6e4e826feb6f62
Андрій Ачілов <andrry1@gmail.com>
2023-12-14 01:10:21 +0200

    Add fetching project data; remove placeholder data.

--- Frontend/src/components/Planner.vue
+++ Frontend/src/components/Planner.vue
@@ -1,105 +1,112 @@
 <template>
-  <h1 class="ms-5 mt-3">Проєкт 1</h1>
-  <form class="container-fluid mt-3 workers">
-    <h4 class="ms-5">Співробітники</h4>
-    <div class="row overflow-x-auto">
-      <div class="col-auto">
-        <table class="table align-middle m-0">
-          <thead class="text-center">
-            <tr>
-              <th v-for="header in workersTableHeaders">
-                {{ header }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <PlannerWorker
-              v-for="(worker, index) in workers"
-              :key="worker.id"
-              :row-index="index + 1"
-              :worker="worker"
-              :dollar-sign="dollarSign"
-              :development-velocity-metric="developmentVelocityMetric"
-              @delete-worker="deleteWorker"
-            />
-            <PlannerNewWorker
-              :row-index="workers.length + 1"
-              :new-worker-id="newWorkerId"
-              :dollar-sign="dollarSign"
-              :development-velocity-metric="developmentVelocityMetric"
-              @add-new-worker="addNewWorker"
-            />
-          </tbody>
-        </table>
+  <div
+    v-if="isLoading"
+    class="spinner-border text-primary position-absolute top-50 start-50"
+    style="width: 3rem; height: 3rem"
+  ></div>
+  <div v-else>
+    <h1 class="ms-5 mt-3">Проєкт "{{ project.name }}"</h1>
+    <form class="container-fluid mt-3 workers">
+      <h4 class="ms-5">Співробітники</h4>
+      <div class="row overflow-x-auto">
+        <div class="col-auto">
+          <table class="table align-middle m-0">
+            <thead class="text-center">
+              <tr>
+                <th v-for="header in workersTableHeaders">
+                  {{ header }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <PlannerWorker
+                v-for="(worker, index) in project.workers"
+                :key="worker.id"
+                :row-index="index + 1"
+                :worker="worker"
+                :dollar-sign="dollarSign"
+                :development-velocity-metric="developmentVelocityMetric"
+                @delete-worker="deleteWorker"
+              />
+              <PlannerNewWorker
+                :row-index="project.workers.length + 1"
+                :new-worker-id="newWorkerId"
+                :dollar-sign="dollarSign"
+                :development-velocity-metric="developmentVelocityMetric"
+                @add-new-worker="addNewWorker"
+              />
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  </form>
-  <form class="container-fluid mt-3 tasks">
-    <h4 class="ms-5">Задачі</h4>
-    <div class="row overflow-x-auto">
-      <div class="col-auto">
-        <table class="table align-middle m-0">
-          <thead class="text-center">
-            <tr>
-              <th v-for="header in tasksTableHeaders">
-                {{ header }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <PlannerTask
-              v-for="(task, index) in tasks"
-              :key="task.id"
-              :row-index="index + 1"
-              :task="task"
-              :tasks="tasks"
-              :workers="workers"
-              :story-points-sign="storyPointsSign"
-              @delete-task="deleteTask"
-              @add-parent-task="addParentTask"
-              @delete-parent-task="deleteParentTask"
-              @add-available-worker="addAvailableWorker"
-              @delete-available-worker="deleteAvailableWorker"
-            />
-            <PlannerNewTask
-              :row-index="tasks.length + 1"
-              :new-task-id="newTaskId"
-              :tasks="tasks"
-              :workers="workers"
-              :story-points-sign="storyPointsSign"
-              @add-new-task="addNewTask"
-            />
-          </tbody>
-        </table>
+    </form>
+    <form class="container-fluid mt-3 tasks">
+      <h4 class="ms-5">Задачі</h4>
+      <div class="row overflow-x-auto">
+        <div class="col-auto">
+          <table class="table align-middle m-0">
+            <thead class="text-center">
+              <tr>
+                <th v-for="header in tasksTableHeaders">
+                  {{ header }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <PlannerTask
+                v-for="(task, index) in project.tasks"
+                :key="task.id"
+                :row-index="index + 1"
+                :task="task"
+                :tasks="project.tasks"
+                :workers="project.workers"
+                :story-points-sign="storyPointsSign"
+                @delete-task="deleteTask"
+                @add-parent-task="addParentTask"
+                @delete-parent-task="deleteParentTask"
+                @add-available-worker="addAvailableWorker"
+                @delete-available-worker="deleteAvailableWorker"
+              />
+              <PlannerNewTask
+                :row-index="project.tasks.length + 1"
+                :new-task-id="newTaskId"
+                :tasks="project.tasks"
+                :workers="project.workers"
+                :story-points-sign="storyPointsSign"
+                @add-new-task="addNewTask"
+              />
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  </form>
-  <div class="container-fluid my-3 actions">
-    <div class="row gy-3">
-      <div class="col-lg-auto">
-        <button
-          type="button"
-          class="btn btn-lg btn-primary w-100"
-          @click="planProjectForMinimalTime"
-        >
-          Cпланувати проєкт за мінімальний час
-        </button>
-      </div>
-      <div class="col-lg-auto">
-        <button
-          type="button"
-          class="btn btn-lg btn-primary w-100"
-          @click="planProjectForMinimalWorkersCount"
-        >
-          Cпланувати проєкт за мінімальну кількість співробітників
-        </button>
+    </form>
+    <div class="container-fluid my-3 actions">
+      <div class="row gy-3">
+        <div class="col-lg-auto">
+          <button
+            type="button"
+            class="btn btn-lg btn-primary w-100"
+            @click="planProject(Modes.MinimalTime)"
+          >
+            Cпланувати проєкт за мінімальний час
+          </button>
+        </div>
+        <div class="col-lg-auto">
+          <button
+            type="button"
+            class="btn btn-lg btn-primary w-100"
+            @click="planProject(Modes.MinimalWorkersCount)"
+          >
+            Cпланувати проєкт за мінімальну кількість співробітників
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, watchEffect, onBeforeMount } from "vue";
+import { ref, computed, watchEffect, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import router from "../router";
 import PlannerWorker from "./PlannerWorker.vue";
@@ -114,61 +121,42 @@ const emit = defineEmits([
   "plan-project-for-minimal-workers-count",
 ]);
 
-const route = useRoute();
-
-const project = ref(null);
-
 const dollarSign = "$";
 const storyPointsSign = "SP";
 const developmentVelocityMetric = `${storyPointsSign}/тиж.`;
+const Modes = {
+  MinimalTime: "MinimalTime",
+  MinimalWorkersCount: "MinimalWorkersCount",
+};
+
+const isLoading = ref(true);
+const route = useRoute();
+
+const project = ref(null);
 
 function backToProjects() {
   emit("back-to-projects");
 }
 
 const workersTableHeaders = ["", "№", "ПІБ", "Зарплата", "Швидкість розробки"];
-const workers = ref([
-  {
-    id: 1,
-    name: "Співробітник 1",
-    salary: 150,
-    developmentVelocity: 15,
-  },
-  {
-    id: 2,
-    name: "Співробітник 2",
-    salary: 100,
-    developmentVelocity: 10,
-  },
-  {
-    id: 3,
-    name: "Співробітник 3",
-    salary: 30,
-    developmentVelocity: 3,
-  },
-  {
-    id: 4,
-    name: "Співробітник 4",
-    salary: 20,
-    developmentVelocity: 2,
-  },
-]);
 
 const newWorkerId = computed(() => {
-  if (workers.value.length === 0) {
+  if (project.value.workers.length === 0) {
     return 1;
   }
 
-  const sortedWorkers = [...workers.value].sort((worker1, worker2) => worker1.id - worker2.id);
+  const sortedWorkers = [...project.value.workers].sort(
+    (worker1, worker2) => worker1.id - worker2.id
+  );
   return sortedWorkers[sortedWorkers.length - 1].id + 1;
 });
 
 function addNewWorker(worker) {
-  workers.value = [...workers.value, worker];
+  project.value.workers = [...project.value.workers, worker];
 }
 
 function deleteWorker(workerId) {
-  workers.value = workers.value.filter((worker) => worker.id != workerId);
+  project.value.workers = project.value.workers.filter((worker) => worker.id != workerId);
 }
 
 const tasksTableHeaders = [
@@ -179,245 +167,101 @@ const tasksTableHeaders = [
   "Залежить від",
   "Доступні співробітники для виконання",
 ];
-const tasks = ref([
-  {
-    id: 1,
-    name: "Задача 1",
-    complexity: 5,
-    parentTasks: [],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 2,
-    name: "Задача 2",
-    complexity: 10,
-    parentTasks: [],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 3,
-    name: "Задача 3",
-    complexity: 2,
-    parentTasks: [],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 4,
-    name: "Задача 4",
-    complexity: 6,
-    parentTasks: [],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 5,
-    name: "Задача 5",
-    complexity: 8,
-    parentTasks: [1],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 6,
-    name: "Задача 6",
-    complexity: 7,
-    parentTasks: [1, 2, 3],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 7,
-    name: "Задача 7",
-    complexity: 1,
-    parentTasks: [3, 4],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 8,
-    name: "Задача 8",
-    complexity: 4,
-    parentTasks: [5],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 9,
-    name: "Задача 9",
-    complexity: 2,
-    parentTasks: [6, 7],
-    availableWorkers: [1, 2, 3, 4],
-  },
-  {
-    id: 10,
-    name: "Задача 10",
-    complexity: 5,
-    parentTasks: [8, 9],
-    availableWorkers: [1, 2, 3, 4],
-  },
-]);
 
 const newTaskId = computed(() => {
-  if (tasks.value.length === 0) {
+  if (project.value.tasks.length === 0) {
     return 1;
   }
 
-  var sortedTasks = [...tasks.value].sort((task1, task2) => task1.id - task2.id);
+  var sortedTasks = [...project.value.tasks].sort((task1, task2) => task1.id - task2.id);
   return sortedTasks[sortedTasks.length - 1].id + 1;
 });
 
 // Видаляє неіснуючі батьківські задачі зі списку батьків кожної з задач
 // і неіснуючих співробітників із списку доступнимх співробітників кожної з задач.
 watchEffect(() => {
-  tasks.value.forEach((task) => {
+  if (isLoading.value) {
+    return;
+  }
+
+  project.value.tasks.forEach((task) => {
     task.parentTasks = task.parentTasks.filter((parentTask) => {
       // Шукає чи є серед усіх задач хоча б одна, яка дорівнює parentTask.
       // Якщо нема - значить parentTask це видалена задача.
-      return tasks.value.some((t) => t.id == parentTask);
+      return project.value.tasks.some((t) => t.id == parentTask);
     });
 
     task.availableWorkers = task.availableWorkers.filter((worker) => {
       // Шукає чи є серед усіх співробітників хоча б один, який дорівнює worker.
       // Якщо нема - значить worker це видалений співробітник.
-      return workers.value.some((w) => w.id == worker);
+      return project.value.workers.some((w) => w.id == worker);
     });
   });
 });
 
 function addNewTask(task) {
-  tasks.value = [...tasks.value, task];
+  project.value.tasks = [...project.value.tasks, task];
 }
 
 function deleteTask(taskId) {
-  tasks.value = tasks.value.filter((task) => task.id != taskId);
+  project.value.tasks = project.value.tasks.filter((task) => task.id != taskId);
 }
 
 function addParentTask(taskId, parentTaskId) {
-  var foundTask = tasks.value.find((task) => task.id == taskId);
+  var foundTask = project.value.tasks.find((task) => task.id == taskId);
   foundTask.parentTasks = [...foundTask.parentTasks, parentTaskId];
 }
 
 function deleteParentTask(taskId, parentTaskId) {
-  var foundTask = tasks.value.find((task) => task.id == taskId);
+  var foundTask = project.value.tasks.find((task) => task.id == taskId);
   foundTask.parentTasks = foundTask.parentTasks.filter((task) => task != parentTaskId);
 }
 
 function addAvailableWorker(taskId, workerId) {
-  var foundTask = tasks.value.find((task) => task.id == taskId);
+  var foundTask = project.value.tasks.find((task) => task.id == taskId);
   foundTask.availableWorkers = [...foundTask.availableWorkers, workerId];
 }
 
 function deleteAvailableWorker(taskId, workerId) {
-  var foundTask = tasks.value.find((task) => task.id == taskId);
+  var foundTask = project.value.tasks.find((task) => task.id == taskId);
   foundTask.availableWorkers = foundTask.availableWorkers.filter((task) => task != workerId);
 }
 
-function planProjectForMinimalTime() {
-  emit("plan-project-for-minimal-time", tasks.value, workers.value);
+function planProject(mode) {
+  if (mode === Modes.MinimalTime) {
+    router.push({
+      path: `/users/${route.params.userId}/projects/${route.params.projectId}/plan`,
+      query: { mode: mode },
+    });
+  } else if (mode === Modes.MinimalWorkersCount) {
+    const expectedProjectDuration = Number.parseInt(
+      prompt("Введіть очікувану тривалість проєкту:"),
+      10
+    );
+    router.push({
+      path: `/users/${route.params.userId}/projects/${route.params.projectId}/plan`,
+      query: { mode: mode, expectedProjectDuration: expectedProjectDuration },
+    });
+  } else {
+    alert(`${mode} є недійсним значенням для способу планування проєкту.`);
+  }
 }
 
-function planProjectForMinimalWorkersCount() {
-  emit("plan-project-for-minimal-workers-count", tasks.value, workers.value);
-}
-
-onBeforeMount(() => {
-  // Get project data
-  console.log(`User id: ${route.params.userId}, project id: ${route.params.projectId}`);
-  project.value = {
-    tasks: [
-      {
-        id: 1,
-        name: "Задача 1",
-        complexity: 5,
-        parentTasks: [],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 2,
-        name: "Задача 2",
-        complexity: 10,
-        parentTasks: [],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 3,
-        name: "Задача 3",
-        complexity: 2,
-        parentTasks: [],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 4,
-        name: "Задача 4",
-        complexity: 6,
-        parentTasks: [],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 5,
-        name: "Задача 5",
-        complexity: 8,
-        parentTasks: [1],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 6,
-        name: "Задача 6",
-        complexity: 7,
-        parentTasks: [1, 2, 3],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 7,
-        name: "Задача 7",
-        complexity: 1,
-        parentTasks: [3, 4],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 8,
-        name: "Задача 8",
-        complexity: 4,
-        parentTasks: [5],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 9,
-        name: "Задача 9",
-        complexity: 2,
-        parentTasks: [6, 7],
-        availableWorkers: [1, 2, 3, 4],
-      },
-      {
-        id: 10,
-        name: "Задача 10",
-        complexity: 5,
-        parentTasks: [8, 9],
-        availableWorkers: [1, 2, 3, 4],
-      },
-    ],
-    workers: [
-      {
-        id: 1,
-        name: "Співробітник 1",
-        salary: 150,
-        developmentVelocity: 15,
-      },
-      {
-        id: 2,
-        name: "Співробітник 2",
-        salary: 100,
-        developmentVelocity: 10,
-      },
-      {
-        id: 3,
-        name: "Співробітник 3",
-        salary: 30,
-        developmentVelocity: 3,
-      },
-      {
-        id: 4,
-        name: "Співробітник 4",
-        salary: 20,
-        developmentVelocity: 2,
-      },
-    ],
-  };
+onMounted(async () => {
+  try {
+    project.value = await fetch(
+      `https://localhost:7229/api/users/${route.params.userId}/projects/${route.params.projectId}`
+    ).then(async (response) => {
+      const res = await response.text();
+      if (!response.ok) {
+        throw new Error(res);
+      }
+      return JSON.parse(res);
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+  isLoading.value = false;
 });
 </script>
 
