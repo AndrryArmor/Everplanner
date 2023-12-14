fe68aff86808b1ed023209ba50dbae10e7b8860b
Андрій Ачілов <andrry1@gmail.com>
2023-12-14 17:53:59 +0200

    Remove exit button and navigation when not logged in.

--- Frontend/src/components/TheNavbar.vue
+++ Frontend/src/components/TheNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-sm shadow bg-gradient bg-light">
     <div class="container-fluid">
-      <router-link to="/login" class="navbar-brand text-success fw-light">
+      <router-link :to="getHomePage()" class="navbar-brand text-success fw-light">
         Everplanner
       </router-link>
       <button
@@ -13,10 +13,10 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="navbar-nav me-auto">
+        <div v-show="route.params.userId" class="navbar-nav me-auto">
           <router-link to="/users/0/projects" class="nav-link">Проєкти</router-link>
         </div>
-        <div class="navbar-nav">
+        <div v-show="route.params.userId" class="navbar-nav">
           <span class="nav-link active user-select-none">Ачілов Андрій</span>
           <router-link to="/login" class="btn btn-outline-danger" role="button">
             Вийти
@@ -28,6 +28,14 @@
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+function getHomePage() {
+  return route.params.userId ? `/users/${route.params.userId}/projects` : "/login";
+}
+</script>
 
 <style scoped lang="scss"></style>
