7cddcd7c3c8baa9b05a8dc1b2b071091a5e5babb
Андрій Ачілов <andrry1@gmail.com>
2023-12-16 03:18:30 +0200

    Remove unused code

--- Backend/Everplanner.WebApi/Dto/ProjectRequestModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Everplanner.WebApi.Dto;
-
-public record ProjectRequestModel(int Id, string Name, IEnumerable<TaskDto> Tasks, IEnumerable<WorkerDto> Workers)
-{
-    public int ExpectedProjectDuration { get; init; } = int.MaxValue;
-};
--- Backend/Everplanner.WebApi/Dto/TaskDto.cs
+++ Backend/Everplanner.WebApi/Dto/TaskDto.cs
@@ -4,14 +4,6 @@ namespace Everplanner.WebApi.Dto;
 
 public record TaskDto(int Id, string Name, int Complexity, IEnumerable<int> ParentTasks, IEnumerable<int> AvailableWorkers)
 {
-    public static TaskDto FromTask(Task task, IEnumerable<Task> tasks)
-    {
-        IEnumerable<int> parentTasks = tasks
-            .Where(t => t.ChildTasks.Contains(task))
-            .Select(t => t.Id);
-        return new TaskDto(task.Id, task.Name, task.Complexity, parentTasks, task.AvailableWorkers.Select(w => w.Id));
-    }
-
     public static TaskDto FromTask(Task task)
     {
         return new TaskDto(task.Id, task.Name, task.Complexity, task.ParentTasks.Select(t => t.Id), 
